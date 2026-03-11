492d02a8f5abbd87baf752d8d67ecb9f347b97fe
vishnu2006965 <vr9259@srmist.edu.in>
2026-03-11 10:49:42 +0530

    [vishnu] Added [DEv]

---
+++ app/src/BookMyStayApp.java
@@ -0,0 +1,15 @@
+public class BookMyStayApp {
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("====================================");
+        System.out.println(" Welcome to Hotel Booking System ");
+        System.out.println(" Version: 1.0 ");
+        System.out.println("====================================");
+
+        // Inform user that application started successfully
+        System.out.println("Application started successfully.");
+
+    }
+}
+
